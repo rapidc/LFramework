1471805f9fdd894143ca0be30aa9c9d7819e9af8
饭炒蛋 <zhouzelong.cn@gmail.com>
2021-05-20 20:32:14 +0800

    Init

---
+++ Editor/Inspector/LabelTextAttributeDrawer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(LabelTextAttribute))]
+public class LabelTextAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        LabelTextAttribute attr = attribute as LabelTextAttribute;
+        label = EditorGUI.BeginProperty(position, label, property);
+        label.text = attr.Text;
+        EditorGUI.PropertyField(position, property, label, property.isExpanded);
+        EditorGUI.EndProperty();
+    }
+}
---
+++ Runtime/Tools/Attributes/LabelTextAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+public class LabelTextAttribute : PropertyAttribute
+{
+    public string Text { get; private set; }
+
+    public LabelTextAttribute(string text)
+    {
+        Text = text;
+    }
+}
